9c6fcfcb03e489e4c913601b2f422b35e22466da
rcon1012 <rcon1012@gmail.com>
2016-02-06 18:19:14 -0500

    import ArrayList to FileThread.java

--- src/FileThread.java
+++ src/FileThread.java
@@ -2,6 +2,7 @@
 
 import java.lang.Thread;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
 import java.io.FileInputStream;
@@ -36,7 +37,37 @@ public class FileThread extends Thread
 				// Handler to list files that this user is allowed to see
 				if(e.getMessage().equals("LFILES"))
 				{
-				    /* TODO: Write this handler */
+				    //Do error handling
+				    if(e.getObjContents().size() < 1) {
+				    	response = new Envelope("FAIL-BADCONTENTS");
+				    }
+				    else {
+				    	if(e.getObjContents().get(0) == null) {
+				    		response = new Envelope("FAIL-BADTOKEN");
+				    	}
+				    	else {
+
+				    		//Prepare output list of file names and retrieve the token from the envelope
+						    ArrayList<String> filteredFiles = new ArrayList<String>();
+						    UserToken tok = (UserToken)e.getObjContents().get(0);
+
+						    //Get all files from the FileServer
+						    ArrayList<ShareFile> all = FileServer.fileList.getFiles();
+
+						    //Go through all files in the server, filter for only those in the right group
+						    for(ShareFile f : all){
+
+						    	if(tok.getGroups().contains(f.getGroup()))
+						    		filteredFiles.add(f.getGroup());
+						    }
+
+						    //form response, write it
+						    response = new Envelope("OK");
+						    response.addObject(filteredFiles);
+						    output.writeObject(response);
+
+				    	}
+				    }   	
 				}
 				if(e.getMessage().equals("UPLOADF"))
 				{
