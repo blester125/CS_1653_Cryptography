e6e41cf570705e80087429d9db4d86679221fc27
Brian <bdl20@pitt.edu>
2016-03-03 15:34:58 -0500

    Generate salt function added to Hasher because salt is only used in hashes

--- src/Hasher.java
+++ src/Hasher.java
@@ -2,11 +2,17 @@ import java.security.MessageDigest;
 import java.security.*;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import java.math.BigInteger;
+
 public class Hasher {
 	public static byte[] hash(Object obj) throws Exception {
 		MessageDigest md = MessageDigest.getInstance("SHA-256", "BC");
 		md.update(obj.toString().getBytes("UTF-8"));
 		return md.digest();
+	}
+
+	public static BigInteger genSalt() {
+		return new BigInteger(256, new SecureRandom());
 	}
 
 	public static void main(String args[]) throws Exception {
@@ -21,5 +27,7 @@ public class Hasher {
 		String hash2 = new String(hash(test2));
 		System.out.println(hash1 + "\n\n");
 		System.out.println(hash2);
+
+		System.out.println(genSalt());
 	}
 }
