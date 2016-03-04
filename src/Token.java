ac5085ccdcc78bef9de01fbf0dd26a78a3c0f7c6
rcon1012 <rcon1012@gmail.com>
2016-03-03 19:24:28 -0500

    Token has a signed hash field of itself for integrity checks

--- src/Token.java
+++ src/Token.java
@@ -1,16 +1,22 @@
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 
 public class Token implements UserToken {
-	
 	private static final long serialVersionUID = 897646161548165146L;
-
 	private String issuer;
 	private String subject;
 	private ArrayList<String> groups;
 	private Date timestamp;
+	private byte[] signedHash;
 	
 	public static final String sentinal = "#";
 	
@@ -32,22 +38,60 @@ public class Token implements UserToken {
 		this.timestamp = timestamp;
 	}
 	
+	/**
+	 * sets the signed hash value of the token
+	 * @param privateKey	private key used for signing
+	 * @return	true on success, false on failure
+	 */
+	public boolean signToken(PrivateKey privateKey) {
+		Signature signature = null;
+		try {
+			signature = Signature.getInstance("SHA256", "BC");
+		} catch (NoSuchAlgorithmException e3) {
+			e3.printStackTrace();
+		} catch (NoSuchProviderException e3) {
+			e3.printStackTrace();
+			return false;
+		}
+	    try {
+			signature.initSign(privateKey, new SecureRandom());
+		} catch (InvalidKeyException e2) {
+			e2.printStackTrace();
+			return false;
+		}
+	    byte[] message = this.toString().getBytes();
+	    try {
+			signature.update(message);
+		} catch (SignatureException e1) {
+			e1.printStackTrace();
+			return false;
+		}
+	    try {
+			this.signedHash = signature.sign();
+		} catch (SignatureException e) {
+			e.printStackTrace();
+			return false;
+		}
+	    return true;
+	}
+	
 	@Override
 	public String getIssuer() {
-		// TODO Auto-generated method stub
 		return this.issuer;
 	}
 
 	@Override
 	public String getSubject() {
-		// TODO Auto-generated method stub
 		return this.subject;
 	}
 
 	@Override
 	public List<String> getGroups() {
-		// TODO Auto-generated method stub
 		return this.groups;
+	}
+	
+	public byte[] getSignedHash() {
+		return this.signedHash;
 	}
 
 	@Override
