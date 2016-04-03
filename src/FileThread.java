affdb4ee8e02489f0336cb58beef11a0ebdbd4e3
Brian Lester <bdl20@pitt.edu>
2016-04-03 15:07:44 -0400

    Prints

--- src/FileThread.java
+++ src/FileThread.java
@@ -286,8 +286,10 @@ public class FileThread extends Thread
 				    		response = new Envelope("FAIL-BADSEQNUM");
 				    	}
 				    	else 
-				    	{	
+				    	{
+						System.out.println("Expecting seqNum: " sequenceNumber + 1);	
 				    		int tempseq = (Integer)e.getObjContents().get(2);
+						System.out.println("Got: " tempseq);
 				    		if(tempseq == sequenceNumber + 1){
 							System.out.println("Sequence number is OK");
 					    		response = new Envelope("FAIL");
