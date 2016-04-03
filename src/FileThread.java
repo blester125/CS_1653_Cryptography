dfbe53864e54243379d19b14df40c243da398d6a
Brian Lester <bdl20@pitt.edu>
2016-04-03 15:09:42 -0400

    compile error

--- src/FileThread.java
+++ src/FileThread.java
@@ -287,7 +287,7 @@ public class FileThread extends Thread
 				    	}
 				    	else 
 				    	{
-						System.out.println("Expecting seqNum: " sequenceNumber + 1);	
+						System.out.println("Expecting seqNum: " + (sequenceNumber + 1));	
 				    		int tempseq = (Integer)e.getObjContents().get(2);
 						System.out.println("Got: " + tempseq);
 				    		if(tempseq == sequenceNumber + 1){
